5e24d9fdb1b2734eec4bae1448b41671f330d699
Xavier Chen <xchen5@stuy.edu>
2017-11-12 14:15:57 -0500

    API!

--- api.py
+++ api.py
@@ -4,11 +4,16 @@ import json, urllib2
 myapp = Flask(__name__)
 u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=RG4u9Rp5uc2i8hVhhZ5BZeq8VAuJwduq7r2INQ6W")
 j = json.loads(u.read())
-#dic = {}
+
+
 @myapp.route('/')
 def root():
     #print j
-    return render_template('base.html', stuff = j["explanation"], picture = j["url"])
+    fdaLink = "https://api.fda.gov/drug/event.json"
+    fdaLink += "?search=receivedate:[20160101+TO+20161231]+AND+patient.drug.medicinalproduct:caffeine"
+    fda = urllib2.urlopen(fdaLink)
+    fdaJSON = json.loads(fda.read())
+    return render_template('base.html', stuff = j["explanation"], picture = j["url"], fdaAPI = "# FDA cases related to caffeine in 2016: " + str(fdaJSON["meta"]["results"]["total"]))
 
 if __name__ == '__main__':
     myapp.debug = True
